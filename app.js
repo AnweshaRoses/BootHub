89f2611a2e1e15ab5827a41e825671798d264860
Anwesha Mishra <anwesharoses@gmail.com>
2022-04-11 13:24:22 +0530

    added dynamic functionality to the colors too

--- app.js
+++ app.js
@@ -109,3 +109,9 @@ menuItems.forEach((item, index) => {
       });
     });
   });
+
+  currentProductColors.forEach((color,index)=>{
+    color.addEventListener("click",()=>{
+      currentProductImg.src=choosenProduct.colors[index].img
+    })
+  })
